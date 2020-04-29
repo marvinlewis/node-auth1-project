dd7c6e25cd0c66b5489731c94756753caf7fe874
marvin lewis <marvinlewis01@gmail.com>
2020-04-29 13:51:54 -0700

    added start script and port env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const server = require('./server');
 
-server.listen(5000, () => console.log('==!! SERVER WORKING !!=='))
+const port = process.env.PORT || 5000
+
+server.listen(port, () => console.log('==!! SERVER WORKING !!=='))
